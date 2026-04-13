be5bdfb7f899087ef3c03d0fd7f686490a4d0457
Anton Sakhanovych <dev@antonsakhanovych.com>
2026-04-13 19:43:52 -0400

    Add polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   resolve: {
     alias: {
       "@lib": resolve(__dirname, "src/lib"),
